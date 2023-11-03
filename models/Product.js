062c6503561bafc67a079c170c4e5217135abfa4
RG-GitUser <rileygaffney3@icloud.com>
2023-11-02 23:16:08 -0300

    Added comments to product.js

--- models/Product.js
+++ models/Product.js
@@ -6,16 +6,16 @@ class Product extends Model {}
 Product.init(
   {
     id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
+      type: DataTypes.INTEGER, // setting our data type to integer so that the ID can can come back as a number.
+      primaryKey: true, // A primary key exists for this product "id".
+      autoIncrement: true, // auto incriment is to "true" in this case so our product ID returns all product categories. 
     },
     product_name: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING,  // setting our data type to string so that we can return data with a series of products
       allowNull: false,
     },
     price: {
-      type: DataTypes.DECIMAL(10, 2),
+      type: DataTypes.DECIMAL(10, 2), // here we are setting our decimal point to stay only two decimals after the total number of digits, which in this case is 10. 
       allowNull: false, 
     },
   },
