3e061ffe534b39f864827990cfe0b6c29c7d4578
RG-GitUser <rileygaffney3@icloud.com>
2023-11-03 00:07:48 -0300

    added comments for id and tag_name

--- models/Tag.js
+++ models/Tag.js
@@ -7,12 +7,12 @@ class Tag extends Model {}
 Tag.init(
   {
     id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
+      type: DataTypes.INTEGER, //id set to integer to return numbers in the id column.
+      primaryKey: true, // a primary key has been assigned to the id. 
       autoIncrement: true,
     },
     tag_name: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING, // set the tag_name to string so that we can return product names from the database. 
       allowNull: false,
     },
 
