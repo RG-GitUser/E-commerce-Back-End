f143fa963d890d99c62c8af40ceb3dd25273ba23
RG-GitUser <rileygaffney3@icloud.com>
2023-11-04 17:43:20 -0300

    added code and comments for finding tag by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,6 +3,8 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
+// retrieving all tags
+
 router.get('/', async (req, res) => {
   try {                           
     const tags = await Tag.findAll({
@@ -15,16 +17,31 @@ router.get('/', async (req, res) => {
     });
 
     res.json(tags);
-  } catch (error) {
+  } catch (error) { //errors are caught here from if any are found in the db 
     console.error(error);
-    res.status(500).json({ error: 'Failed to retrieve tags' });
+    res.status(500).json({ error: 'Failed to retrieve tags' }); // sending 500 internal server error response with error message 
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+//finding a single tag by its id 
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, { //finding by primary key
+      include: [{ model: Product }],  // including associated prodcut data
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id' }); // 404 status returned if not found
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {             // status 200 code is given for a successful response
+    res.status(500).json(err); // internal server error
+  }
 });
+
+
 
 router.post('/', (req, res) => {
   // create a new tag
