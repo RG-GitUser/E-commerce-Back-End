d6d82509f7e18ab7526a879acc9c09826f1cccc0
RG-GitUser <rileygaffney3@icloud.com>
2023-11-05 12:43:35 -0400

    attempting to fix connection to server

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,32 @@
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
+const sequelize = new Sequelize({
+  dialect: 'mysql',
+  host: 'localhost',
+  username: 'root',
+  password: 'UNBbootcamp!23',
+  database: 'ecommerce_db',
+});
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+module.exports = { sequelize };
 
-module.exports = sequelize;
+
+
+
+
+// require('dotenv').config();
+
+// const Sequelize = require('sequelize');
+
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
+
+// module.exports = sequelize;
